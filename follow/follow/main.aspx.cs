74e41d3a9e734bb516e3e6c4870e672d0b6f8d1d
yuvaloni <yuvon2@gmail.com>
2014-08-04 12:51:48 +0300

    tdre

--- follow/follow/main.aspx.cs
+++ follow/follow/main.aspx.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Net.Mail;
+using System.Net;
 namespace follow
 {
     public partial class main : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write(HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
+            var fromAddress = new MailAddress("bet.frankfurt.newsletter@gmail.com", "בית פרנקפורט");
+            const string fromPassword = "1wJxMrhhBGwKxk2HNkk_xQ";
+            SmtpClient c = new SmtpClient
+            {
+                Host = "smtp.mandrillapp.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            }; 
+            var message = new MailMessage(fromAddress, toAddress)
+                        {
+                            Subject = "ip",
+                            Body =(HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]
+                        };
+                        c.Send(message);
+                    }
+            
         }
     }
 }
